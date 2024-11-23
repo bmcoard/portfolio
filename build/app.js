7acd0480d41d2978f62b7d0dadac3f391963539d
bmcoard <bmcoard@gmail.com>
2024-11-22 21:31:41 -0500

    build

--- build/app.js
+++ build/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const startDatabase = require("./services/databaseService");
 const contactsRouter = require("./api/submit");
@@ -9,15 +18,20 @@ const CORS = require("cors");
 const express = require("express"); //returns function from node module "express"
 const app = express();
 const path = require("path");
-startDatabase();
 app.use(CORS()); //content origin resource sharing - gives permission to have domains access server
 app.use("/api", contactsRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/portfolio", portfolioRouter);
-const PORT = process.env.PORT || 3003; //heroku
-app.listen(PORT, () => {
-    console.log("server running on port 3003");
-});
+function startApp() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield startDatabase();
+        const PORT = process.env.PORT || 3003; //heroku
+        app.listen(PORT, () => {
+            console.log("server running on port 3003");
+        });
+    });
+}
+startApp();
 const staticDirectory = path.join(__dirname, "static");
 app.use(express.static(staticDirectory));
 app.get("/admin", (request, response) => {
